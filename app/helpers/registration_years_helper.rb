96ccf6225ea637e2c4f2f250c8447483dd4e4b21
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-06-23 02:09:17 +0530

    helper files deleted

--- app/helpers/registration_years_helper.rb
+++
@@ -1,2 +0,0 @@
-module RegistrationYearsHelper
-end
--- spec/models/kilometer_driven_spec.rb
+++ spec/models/kilometer_driven_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe KilometerDriven, type: :model do
-	let(:kilometer_driven) { build(:kilometer_driven) } 
-	let(:kilometer_driven_duplicate) { build(:kilometer_driven) }  
-  
-  describe "Validation" do 
+		let(:kilometer_driven) { build(:kilometer_driven) } 
+		let(:kilometer_driven_duplicate) { build(:kilometer_driven) }  
+	
+	describe "Validation" do 
 		it 'Kilometer driven cannot be blank' do 
-			kilometer_driven.name = nil
-			expect(kilometer_driven).to_not be_valid
+				kilometer_driven.name = nil
+				expect(kilometer_driven).to_not be_valid
 		end
 
 		it 'present of kilometer_driven ' do 
-			expect(kilometer_driven).to be_valid
+				expect(kilometer_driven).to be_valid
 		end
 
 		it 'kilometer_driven should be unique' do
-			kilometer_driven.save
-	    expect(kilometer_driven_duplicate).to_not be_valid
-    end
+				kilometer_driven.save
+		expect(kilometer_driven_duplicate).to_not be_valid
+		end
 	end
 
 	describe "Association" do
